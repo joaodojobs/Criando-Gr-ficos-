22fb2323a8b33c8703e9f042c1daf53f7a7c21ce
joaodojobs <163072059+joaodojobs@users.noreply.github.com>
2024-11-25 21:58:43 -0300

    25/11/2024

--- graficos/RedesFavoritasMinhaEscola.js
+++ graficos/RedesFavoritasMinhaEscola.js
@@ -4,19 +4,18 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
    const res = await fetch(url)
    const dados = await res.json()
-   const pessoasconectadas = (dados.total_pessoas_conectadas / 1e9)
-   const pessoasnomundo = (dados.total_pessoas_mundo / 1e9)
+   const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+   const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
    const horas = parseInt(dados.tempo_medio)
-   const minutos = Math.round((dados.tempo_medio-horas) * 100)
-   const porcentagemconectadas = ((pessoasconectadas / pessoasnomundo) * 100).toFixed(2)
-
-
+   const minutos = Math.round((dados.tempo_medio - horas)*100)
+   const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+  
    const paragrafo = document.createElement('p')
    paragrafo.classList.add('graficos-container__texto')
    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
   
-   const container = document.getElementById('graficos-container')
+   const container = document.getElementById(graficos-container)
    container.appendChild(paragrafo)
  }
   vizualizarInformacoesGlobais()
- 
+
